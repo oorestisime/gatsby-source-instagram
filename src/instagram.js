b64a9f0673deb70095a3258e1158cfb46a695eec
Owen <62473795+theowenyoung@users.noreply.github.com>
2020-04-15 16:26:07 +0800

    fix: there is only one page data (#142)
    * fix: there is only one page data
    
    * docs: add comment for maxPosts logic

--- src/instagram.js
+++ src/instagram.js
@@ -100,9 +100,10 @@ export async function apiInstagramPosts({
     .then(async response => {
       const results = []
       results.push(...response.data.data)
+      // if maxPosts option specified, then check if there is a next field in the response data and the results' length <= maxPosts
+      // otherwise, fetch as more as it can
       while (
-        response.data.paging.next ||
-        (maxPosts && results.length <= maxPosts)
+        maxPosts ? (response.data.paging.next && results.length <= maxPosts) : response.data.paging.next
       ) {
         response = await axios(response.data.paging.next)
         results.push(...response.data.data)
