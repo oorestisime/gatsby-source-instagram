1504f6109941eb6a8ab5c87490ef72a38c68e6f2
Joel <joel@joelmturner.com>
2020-08-05 09:58:16 -0700

    add `hashtags` to example project

--- example/src/pages/index.js
+++ example/src/pages/index.js
@@ -23,6 +23,7 @@ export const pageQuery = graphql`
           likes
           caption
           comments
+          hashtags
           localFile {
             childImageSharp {
               fluid(quality: 70, maxWidth: 600, maxHeight: 600) {
